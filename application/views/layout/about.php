4d065252f0c7092f200a64888ec00893229de9cf
DoraemonOppa <registristathea@gmail.com>
2018-01-17 13:32:35 +0800

    change about

--- application/views/layout/about.php
+++ application/views/layout/about.php
@@ -6,11 +6,17 @@
             <div class="col-xl-9 col-lg-10 mx-auto">
               <div class="bg-faded rounded p-5">
                 <h2 class="section-heading mb-4">
-                  <span class="section-heading-upper">Coffee, Foods and Merchandises</span>
-                  <span class="section-heading-lower">About Our Cafe</span>
+                  <span class="section-heading-upper">About Our Cafe</span>
                 </h2>
                 <p>Founded in <em>2018</em> by the <em>STI MAWD students</em>, our  cafe is not only about coffee but also about foods and merchandises that we want to promote. This cafe was inspired by our own imaginations and like other cafe here in the Philippines, we have our own mission and vision. We are dedicated to travelling the world, finding the best coffee, and bringing back to you here in our cafe.</p>
-                
+                <h3>
+                  <span class="section-heading-upper">Mission</span>
+                </h3>
+                <p>The Marshweet’s main objective is to produce a product with good qualities and to promote their cafe here in Iligan city, and to inspire the youth to be a successful entrepreneur in the future.</p>
+                <h4>
+                  <span class="section-heading-upper">Vision</span>
+                </h4>
+                <p>The vision of Marshweet is to become well-known in cafe and manufacturer field not just in the Philippines but also in other countries with the superior confidence in becoming successful.</p>
               </div>
             </div>
           </div>
