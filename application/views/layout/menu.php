b59a9483a31b444fb2fd9bd8a1b9780defa4672d
DoraemonOppa <registristathea@gmail.com>
2018-01-17 15:19:09 +0800

    change menu

--- application/views/layout/menu.php
+++ application/views/layout/menu.php
@@ -12,7 +12,7 @@
           <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="<?php echo base_url('themes/img/products-01.jpg');?>" alt="">
           <div class="product-item-description d-flex mr-auto">
             <div class="bg-faded p-5 rounded">
-              <p class="mb-0">Taste our rich flavorable dessert, featuring locally roasted coffee beans.Also, we take pride to our works.Every time you order a beverage from us, we guarantee that it will be an experience worth having. Whether it's our world famous Venezuelan Cappuccino, Latte, Mocha, Espresso, american or something as simple as a cup of specialty sourced black coffee.</p>
+              <p class="mb-0">Taste our rich flavorable dessert, featuring locally roasted coffee beans.Also, we take pride to our works.Every time you order a beverage from us, we guarantee that it will be an experience worth having. Whether it's our world famous Venezuelan Cappuccino, Latte, Mocha, Espresso, American or something as simple as a cup of specialty sourced black coffee.</p>
             </div>
           </div>
         </div>
@@ -26,7 +26,7 @@
             <div class="bg-faded p-5 d-flex mr-auto rounded">
               <h2 class="section-heading mb-0">
                 <span class="section-heading-upper">Delicious Treats, Good Eats</span>
-                <span class="section-heading-lower">Bakery &amp; Kitchen</span>
+                <span class="section-heading-lower">Dessert &amp; Toppings</span>
               </h2>
             </div>
           </div>
@@ -46,8 +46,29 @@
             <div class="product-item-title d-flex">
               <div class="bg-faded p-5 d-flex ml-auto rounded">
                 <h2 class="section-heading mb-0">
-                  <span class="section-heading-upper">From Around the World</span>
-                  <span class="section-heading-lower">Bulk Speciality Blends</span>
+                  <span class="section-heading-upper">From The Phillipines</span>
+                  <span class="section-heading-lower">Shakes</span>
+                </h2>
+              </div>
+            </div>
+            <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="<?php echo base_url('themes/img/products-03.jpg');?>" alt="">
+            <div class="product-item-description d-flex mr-auto">
+              <div class="bg-faded p-5 rounded">
+                <p class="mb-0">Travelling the world for the very best quality coffee is something take pride in. When you visit us, you'll always find new blends from around the world, mainly from regions in Central and South America. We sell our blends in smaller to large bulk quantities. Please visit us in person for more details.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="page-section">
+        <div class="container">
+          <div class="product-item">
+            <div class="product-item-title d-flex">
+              <div class="bg-faded p-5 d-flex ml-auto rounded">
+                <h2 class="section-heading mb-0">
+                  <span class="section-heading-upper">Asians Best</span>
+                  <span class="section-heading-lower">Merchandises</span>
                 </h2>
               </div>
             </div>
