1d6653ae9a6dbae650a25b3ee2c6b4dac99b82ac
keim <keim.flor@gmail.com>
2018-01-17 09:35:09 -0800

    Change Title

--- application/views/layout/title.php
+++ application/views/layout/title.php
@@ -1,5 +1,5 @@
 
     <h1 class="site-heading text-center text-white d-none d-lg-block">
       <span class="site-heading-upper text-primary mb-3">MarshSweet Cafe</span>
-      <span class="site-heading-lower">Sing me to sleep now</span>
+      <span class="site-heading-lower">Test Change</span>
     </h1>
