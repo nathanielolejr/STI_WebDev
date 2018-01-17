be1112673e8226f262af2dc18d03eb3504bb0e40
DoraemonOppa <registristathea@gmail.com>
2018-01-17 13:00:30 +0800

    change about

--- application/views/layout/about.php
+++ application/views/layout/about.php
@@ -6,10 +6,10 @@
             <div class="col-xl-9 col-lg-10 mx-auto">
               <div class="bg-faded rounded p-5">
                 <h2 class="section-heading mb-4">
-                  <span class="section-heading-upper">Strong Coffee, Strong Roots</span>
+                  <span class="section-heading-upper">Coffee, Foods and Merchandises</span>
                   <span class="section-heading-lower">About Our Cafe</span>
                 </h2>
-                <p>Founded in 1987 by the Hernandez brothers, our establishment has been serving up rich coffee sourced from artisan farmers in various regions of South and Central America. We are dedicated to travelling the world, finding the best coffee, and bringing back to you here in our cafe.</p>
+                <p>Founded in 2018 by the STI MAWD students, our  cafe is not only about coffee but also about foods and merchandises that we want to promote. This cafe was inspired by our own imaginations and like other cafe here in the Philippines, we have our own mission and vision. We are dedicated to travelling the world, finding the best coffee, and bringing back to you here in our cafe.</p>
                 <p class="mb-0">We guarantee that you will fall in <em>lust</em> with our decadent blends the moment you walk inside until you finish your last sip. Join us for your daily routine, an outing with friends, or simply just to enjoy some alone time.</p>
               </div>
             </div>
