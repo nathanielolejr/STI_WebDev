78a79dde3fadef4312450f5f415245902badc655
shamae17 <prettykrisha20@gmail.com>
2018-01-17 13:55:52 +0800

    change Home

--- application/views/layout/home.php
+++ application/views/layout/home.php
@@ -4,10 +4,10 @@
           <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="<?php echo base_url('themes/img/intro.jpg'); ?>" alt="">
           <div class="intro-text left-0 text-center bg-faded p-5 rounded">
             <h2 class="section-heading mb-4">
-              <span class="section-heading-upper">Fresh Coffee</span>
-              <span class="section-heading-lower">Worth Drinking</span>
+              <span class="section-heading-upper">Marshweet Cafe is Always a GOOD CHOICE</span>
+          
             </h2>
-            <p class="mb-3">Every cup of our quality artisan coffee starts with locally sourced, hand picked ingredients. Once you try it, our coffee will be a blissful addition to your everyday morning routine - we guarantee it!
+            <p class="mb-3">Relax and fresh your mind with Marshweet Cafe
             </p>
             <div class="intro-button mx-auto">
               <a class="btn btn-primary btn-xl" href="#">Visit Us Today!</a>
@@ -26,7 +26,7 @@
                 <span class="section-heading-upper">Our Promise</span>
                 <span class="section-heading-lower">To You</span>
               </h2>
-              <p class="mb-0">When you walk into our shop to start your day, we are dedicated to providing you with friendly service, a welcoming atmosphere, and above all else, excellent products made with the highest quality ingredients. If you are not satisfied, please let us know and we will do whatever we can to make things right!</p>
+              <p class="mb-0">is to provide a good service with a friendly atmosphere that can relax your mind and tummy that will satisfy your palate.</p>
             </div>
           </div>
         </div>
