11754f9d6b9ed1481f0008271618761fe250df67
WarriorPraise <praisewarriorkane@gmail.com>
2018-01-17 15:31:49 +0800

    Contact and footer

--- application/views/layout/contact.php
+++ application/views/layout/contact.php
@@ -51,23 +51,12 @@
                   <br>
                   Iligan City, Lanao del Norte
                 </em>
-                              
-                <div id="map" style="width:400px;height:400px;background:black"></div>
-                <script>
-                function myMap() {
-                var mapOptions = {
-                  center: new google.maps.LatLng(51.5, -0.12),
-                  zoom: 10,
-                  mapTypeId: google.maps.MapTypeId.HYBRID
-                }
-                var map = new google.maps.Map(document.getElementById("map"), mapOptions)
-                }
-                </script>
-                <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDRx4TZWfYjIrlRfLhNTZH5M3ki_UJvmJ4&callback=initMap">
-                </script>
-                
+
+                <div class="map-responsive">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d415.0593345281339!2d124.23968692053482!3d8.229671614937455!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zOMKwMTMnNDQuNSJOIDEyNMKwMTQnMjAuMyJF!5e0!3m2!1sen!2sph!4v1516173885559" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                </div>
+
               </p>
-              
             </div>
           </div>
         </div>
