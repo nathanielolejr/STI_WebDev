b9bc3809e5bdd7fbccbd05d9367c34b8cf5b6615
shamae17 <prettykrisha20@gmail.com>
2018-01-17 15:45:39 +0800

    change menu

--- application/views/layout/menu.php
+++ application/views/layout/menu.php
@@ -12,7 +12,7 @@
           <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="<?php echo base_url('themes/img/products-01.jpg');?>" alt="">
           <div class="product-item-description d-flex mr-auto">
             <div class="bg-faded p-5 rounded">
-              <p class="mb-0">Taste our rich flavorable dessert, featuring locally roasted coffee beans.Also, we take pride to our works.Every time you order a beverage from us, we guarantee that it will be an experience worth having. Whether it's our world famous Venezuelan Cappuccino, Latte, Mocha, Espresso, american or something as simple as a cup of specialty sourced black coffee.</p>
+              <p class="mb-0">Taste our rich flavorful dessert, featuring locally roasted coffee beans. Thus, we take pride in our work.Every time you order a beverage from us, we can guarantee that it will be an experience worth having. Whether it's our world famous Venezuelan Cappuccino, Latte, Mocha, Espresso, american or something as simple as a cup of specialty sourced black coffee.</p>
             </div>
           </div>
         </div>
@@ -26,14 +26,14 @@
             <div class="bg-faded p-5 d-flex mr-auto rounded">
               <h2 class="section-heading mb-0">
                 <span class="section-heading-upper">Delicious Treats, Good Eats</span>
-                <span class="section-heading-lower">Dessert &amp; Toppings/span>
+                <span class="section-heading-lower">Dessert &amp; Toppings</span>
               </h2>
             </div>
           </div>
           <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="<?php echo base_url('themes/img/products-02.jpg');?>" alt="">
           <div class="product-item-description d-flex ml-auto">
             <div class="bg-faded p-5 rounded">
-              <p class="mb-0">Our seasonal menu features delicious snacks, baked goods, and even full meals perfect for breakfast or lunchtime. We source our ingredients from local, oragnic farms whenever possible, alongside premium vendors for specialty goods.</p>
+              <p class="mb-0">You will never fully satisfy your own desires unless a coffee comes with something sweet - a dessert, made by our best chefs that will melt your tongue in its perfect harmonious blend. Sprinkled with toppings that will complete the formula of perfection.</p>
             </div>
           </div>
         </div>
