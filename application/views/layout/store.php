14b2912d64041cb645dceaf7dfdb4bcebd9bf71c
WarriorPraise <praisewarriorkane@gmail.com>
2018-01-17 13:49:12 +0800

    changes in contact

--- application/views/layout/store.php
+++ application/views/layout/store.php
@@ -44,20 +44,24 @@
                   Iligan City, Lanao del Norte
                 </em>
                 <br>
+                <br>
+                <br>
                 <em>
                 Location:
+                <br>
                 <div id="map" style="width:400px;height:400px;background:yellow"></div>
                 <script>
-                  function myMap() {
-                  var mapOptions = {
-                         center: new google.maps.LatLng(8.229016, 124.238993),
-                          zoom: 20,
-                        mapTypeId: google.maps.MapTypeId.HYBRID
-                        }
-                  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-                  }
+                function myMap() {
+                var mapOptions = {
+                  center: new google.maps.LatLng(51.5, -0.12),
+                  zoom: 10,
+                  mapTypeId: google.maps.MapTypeId.HYBRID
+                }
+                var map = new google.maps.Map(document.getElementById("map"), mapOptions)
+                }
                 </script>
-                <script src="https://maps.googleapis.com/maps/api/js?key= AIzaSyDRx4TZWfYjIrlRfLhNTZH5M3ki_UJvmJ4&callback=myMap"></script>
+                <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDRx4TZWfYjIrlRfLhNTZH5M3ki_UJvmJ4&callback=initMap"></script>
+                </iframe>
                 </em>
 
               </p>
