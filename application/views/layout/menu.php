37192bd6d7dbf592800c79112c2b5de21a1082e2
DoraemonOppa <registristathea@gmail.com>
2018-01-17 15:33:15 +0800

    change menu

--- application/views/layout/menu.php
+++ application/views/layout/menu.php
@@ -5,14 +5,14 @@
             <div class="bg-faded p-5 d-flex ml-auto rounded">
               <h2 class="section-heading mb-0">
                 <span class="section-heading-upper">Blended to Perfection</span>
-                <span class="section-heading-lower">Coffees</span>
+                <span class="section-heading-lower">Coffee</span>
               </h2>
             </div>
           </div>
           <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="<?php echo base_url('themes/img/products-01.jpg');?>" alt="">
           <div class="product-item-description d-flex mr-auto">
             <div class="bg-faded p-5 rounded">
-              <p class="mb-0">Taste our rich flavorable dessert, featuring locally roasted coffee beans.Also, we take pride to our works.Every time you order a beverage from us, we guarantee that it will be an experience worth having. Whether it's our world famous Venezuelan Cappuccino, Latte, Mocha, Espresso, American or something as simple as a cup of specialty sourced black coffee.</p>
+              <p class="mb-0">Taste our rich flavorful dessert, featuring locally roasted coffee beans.Also, we take pride to our works.Every time you order a beverage from us, we guarantee that it will be an experience worth having. Whether it's our world famous Venezuelan Cappuccino, Latte, Mocha, Espresso, American or something as simple as a cup of specialty sourced black coffee.</p>
             </div>
           </div>
         </div>
@@ -61,24 +61,24 @@
         </div>
       </section>
 
-      <section class="page-section">
-        <div class="container">
-          <div class="product-item">
-            <div class="product-item-title d-flex">
-              <div class="bg-faded p-5 d-flex ml-auto rounded">
-                <h2 class="section-heading mb-0">
-                  <span class="section-heading-upper">Asians Best</span>
-                  <span class="section-heading-lower">Merchandises</span>
-                </h2>
-              </div>
+<section class="page-section">
+      <div class="container">
+        <div class="product-item">
+          <div class="product-item-title d-flex">
+            <div class="bg-faded p-5 d-flex mr-auto rounded">
+              <h2 class="section-heading mb-0">
+                <span class="section-heading-upper">Asians Best</span>
+                <span class="section-heading-lower">Merchandises</span>
+              </h2>
             </div>
-            <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="<?php echo base_url('themes/img/products-03.jpg');?>" alt="">
-            <div class="product-item-description d-flex mr-auto">
-              <div class="bg-faded p-5 rounded">
-                <p class="mb-0">Travelling the world for the very best quality coffee is something take pride in. When you visit us, you'll always find new blends from around the world, mainly from regions in Central and South America. We sell our blends in smaller to large bulk quantities. Please visit us in person for more details.</p>
-              </div>
+          </div>
+          <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="<?php echo base_url('themes/img/products-02.jpg');?>" alt="">
+          <div class="product-item-description d-flex ml-auto">
+            <div class="bg-faded p-5 rounded">
+              <p class="mb-0">Our seasonal menu features delicious snacks, baked goods, and even full meals perfect for breakfast or lunchtime. We source our ingredients from local, oragnic farms whenever possible, alongside premium vendors for specialty goods.</p>
             </div>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
       
