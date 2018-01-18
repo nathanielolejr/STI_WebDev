96b03342914c2b864d0ca72468999d160c0a13f7
WarriorPraise <praisewarriorkane@gmail.com>
2018-01-18 11:15:42 +0800

    Title

--- application/views/layout/title.php
+++ application/views/layout/title.php
@@ -1,6 +1,7 @@
-<div>
-    <h1 class="site-heading text-center text-white d-none d-lg-block">
-    	<img src="<?php echo base_url('themes/img/Marshweet.png');?>">
-      <span class="site-heading-lower">Marshweet Café</span>
-    </h1>
-</div>
+<head>
+<link href="<?php echo base_url('themes/css/title.css'); ?>" rel="stylesheet">
+</head> 
+      <h1 class="site-heading text-center text-white d-none d-lg-block">      
+            <img class="imagelogo2" src="<?php echo base_url('themes/img/Title.png');?>">
+      </h1>
+      
