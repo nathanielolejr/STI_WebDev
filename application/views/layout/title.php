31cf52378ba02fee2da7bd0061f798888a7b0096
WarriorPraise <praisewarriorkane@gmail.com>
2018-01-18 16:31:59 +0800

    database

--- application/views/layout/title.php
+++ application/views/layout/title.php
@@ -1,7 +1,9 @@
 <head>
 <link href="<?php echo base_url('themes/css/title.css'); ?>" rel="stylesheet">
 </head> 
-      <h1 class="site-heading text-center text-white d-none d-lg-block">      
+      <h1 class="site-heading text-center text-white d-none d-lg-block">
+      <a class="" href="<?php echo base_url('welcome/index');?>">  
             <img class="imagelogo2" src="<?php echo base_url('themes/img/Title.png');?>">
+      </a>  
       </h1>
       
