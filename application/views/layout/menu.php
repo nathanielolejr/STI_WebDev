c93cf7b3da6ed669d42cd4d44d0683856e81b9b1
DoraemonOppa <registristathea@gmail.com>
2018-01-18 10:58:52 +0800

    changed

--- application/views/layout/menu.php
+++ application/views/layout/menu.php
@@ -27,14 +27,14 @@
             <div class="bg-faded p-5 d-flex mr-auto rounded">
               <h2 class="section-heading mb-0">
                 <span class="section-heading-upper">Delicious Treats, Good Eats</span>
-                <span class="section-heading-lower">Dessert &amp; Toppings</span>
+                <span class="section-heading-lower">Dessert</span>
               </h2>
             </div>
           </div>
           <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="<?php echo base_url('themes/img/4e1b2a8955f677f914c515f3649cb523--waffle-toppings-crepe.jpg');?>" alt="">
           <div class="product-item-description d-flex ml-auto">
             <div class="bg-faded p-5 rounded">
-              <p class="mb-0">You will never fully satisfy your own desires unless a coffee comes with something sweet - a dessert, made by our best chefs that will melt your tongue in its perfect harmonious blend. Sprinkled with toppings that will complete the formula of perfection.</p>
+              <p class="mb-0">You will never fully satisfy your own desires unless a coffee comes with something sweet - a dessert, made by our best chefs that will melt your tongue in its perfect harmonious blend. Take your obsession to the next level with these jaw-dropping desserts that were made for coffee lovers. Whether you're layering coffee cream into mousse pots, adding espresso to rich chocolate cake batter or actually making mini cappuccino cups from coffee-flavoured sponge, we've got the recipes to give you the buzz you're looking for.</p>
             </div>
           </div>
         </div>
@@ -48,14 +48,15 @@
               <div class="bg-faded p-5 d-flex ml-auto rounded">
                 <h2 class="section-heading mb-0">
                   <span class="section-heading-upper">From The Philippines</span>
-                  <span class="section-heading-lower">Shakes</span>
+                  <span class="section-heading-lower">Shakes &amp; Toppings</span>
                 </h2>
               </div>
             </div>
             <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="<?php echo base_url('themes/img/shakes.jpg');?>" alt="">
+            <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="<?php echo base_url('themes/img/shakes.jpg');?>" alt="">
             <div class="product-item-description d-flex mr-auto">
               <div class="bg-faded p-5 rounded">
-                <p class="mb-0"> When it comes to ice cream treats, our milkshakes are the coolest. They're cold and creamy and the best of all. A delight creamy milkshakes to pick up on those hot days. Whether it is a Fresh strawbery Milk shake with cherry on top, Vanilla, Mocha, Melted Chocolate, Oreo, Banana milk shake.</p>
+                <p class="mb-0"> When it comes to ice cream treats, our milkshakes are the coolest. They're cold and creamy and the best of all. A delight creamy milkshakes to pick up on those hot days. Whether it is a Fresh strawbery Milk shake with cherry on top, Vanilla, Mocha, Melted Chocolate, Oreo, Banana milk shake. Sprinkled with toppings that will complete the formula of perfection.</p>
               </div>
             </div>
           </div>
@@ -68,12 +69,12 @@
           <div class="product-item-title d-flex">
             <div class="bg-faded p-5 d-flex mr-auto rounded">
               <h2 class="section-heading mb-0">
-                <span class="section-heading-upper">Asians Best</span>
+                <span class="section-heading-upper">Beautiful Treats</span>
                 <span class="section-heading-lower">Merchandise</span>
               </h2>
             </div>
           </div>
-          <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="<?php echo base_url('themes/img/Chains.jpg');?>" alt="">
+          <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="<?php echo base_url('themes/img/Merchandise.jpg');?>" alt="">
           <div class="product-item-description d-flex ml-auto">
             <div class="bg-faded p-5 rounded">
               <p class="mb-0">Buy certain of products to get one of this merchandise for free.</p>
