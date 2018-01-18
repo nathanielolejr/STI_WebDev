8994a202999479bf0c497da4ff5cc7c12206caab
shamae17 <prettykrisha20@gmail.com>
2018-01-18 16:01:15 +0800

    change home

--- application/views/layout/home.php
+++ application/views/layout/home.php
@@ -5,8 +5,8 @@
           <div class="intro-text left-0 text-center bg-faded p-5 rounded">
             <h2 class="section-heading mb-4">Enjoy the serene sensation 
             </h2>
-            <p class="mb-3">Relax and refresh your mind with Marshweet Café
-            </p>
+            <i class="mb-3">You love our coffee. We love our guest
+            </i>
             <div class="intro-button mx-auto">
               <a class="btn btn-primary btn-xl" href="#">Visit Us Today!</a>
             </div>
