1fbb94ce77556f891cf134a9af693ff5bef3428c
WarriorPraise <praisewarriorkane@gmail.com>
2018-01-19 10:38:39 +0800

    title changes

--- application/views/layout/title.php
+++ application/views/layout/title.php
@@ -1,6 +1,5 @@
 <head>
 <link href="<?php echo base_url('themes/css/title.css'); ?>" rel="stylesheet">
-<link href="<?php echo base_url('themes/css/boots.css'); ?>" rel="stylesheet">
 </head> 
       <h1 class="site-heading text-center text-white d-none d-lg-block">
       <a class="" href="<?php echo base_url('welcome/index');?>">  
