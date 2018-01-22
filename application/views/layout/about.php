ade651bbe0e5784b98c9ac7ff8cc09ef633fbb6f
DoraemonOppa <registristathea@gmail.com>
2018-01-22 08:54:04 +0800

    changed text in about

--- application/views/layout/about.php
+++ application/views/layout/about.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <section class="page-section about-heading">
       <div class="container">
         <img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="<?php echo base_url('themes/img/views.jpg');?>" alt="">
@@ -13,7 +12,7 @@
                 <h2>
                   <span class="section-heading-upper">Mission</span>
                 </h2>
-                <p>We aim to serve the best quality and most interesting varieties of coffee, shakes and dessert and to produce a product with good qualities and to promote café here in our community <em>Iligan City, Philippines</em> and also for us to inspire youth to be a successful entrepreneur in the near future.</p>
+                <p>We aim to serve the best quality and most interesting varieties of coffee, shakes and dessert and to produce a product with good qualities and to promote café here in our community <em>Iligan City</em> and also for us to inspire youth to be a successful entrepreneur in the near future.</p>
                 <h2>
                   <span class="section-heading-upper">Vision</span>
                 </h2>
@@ -23,30 +22,4 @@
           </div>
         </div>
       </div>
-=======
-<section class="page-section about-heading">
-      <div class="container">
-        <img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="<?php echo base_url('themes/img/views.jpg');?>" alt="">
-        <div class="about-heading-content">
-          <div class="row">
-            <div class="col-xl-9 col-lg-10 mx-auto">
-              <div class="bg-faded rounded p-5">
-                <h2>
-                  <span class="section-heading-upper">About Our Café</span>
-                </h2>
-                <p>Founded in <em>2018</em> by the <em>STI MAWD students</em>, our  café is not only about coffee but also about foods and merchandises that we want to promote. This café was inspired by our own imaginations and like other cafe here in the Philippines, we have our own mission and vision. We are dedicated to travelling the world, finding the best coffee, and bringing back to you here in our café.</p>
-                <h2>
-                  <span class="section-heading-upper">Mission</span>
-                </h2>
-                <p>We aim to serve the best quality and most interesting varieties of coffee, shakes and dessert and to produce a product with good qualities and to promote café here in our community "Iligan City, Philippines" and also for us to inspire youth to be a successful entrepreneur in the near future.</p>
-                <h2>
-                  <span class="section-heading-upper">Vision</span>
-                </h2>
-                <p>Marshweet Café will make it's best effort to create a unique place where costumers can socialized with each other in a comfortable and relaxing environment while enjoying the best brewed coffee. We will be in the business of helping our costumers to relieve their daily stresses by providing piece of mind through great ambience, convenient location,friendly costumer service, and products of consistently high-quality. Enable to become a well-known café and manufacturer field not just in the Philippines but also in other countries with the superior confidence in becoming successful.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
->>>>>>> 5bac23ac5106cc8604e79eb2808190b2d0c68673
     </section>
