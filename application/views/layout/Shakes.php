dcd16bc0a835e2917b9eefb06ee3fb3d171bfbcf
DoraemonOppa <registristathea@gmail.com>
2018-01-23 13:28:24 +0800

    added images in menu

--- application/views/layout/Shakes.php
+++ application/views/layout/Shakes.php
@@ -1,87 +1,69 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-  <div class="container">
-  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-      <li data-target="#myCarousel" data-slide-to="3"></li>
-      <li data-target="#myCarousel" data-slide-to="4"></li>
-      <li data-target="#myCarousel" data-slide-to="5"></li>
-    </ol>
+<div class="row">
+  <div class="col-md-6">
+    <div class="page-section cta">
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-9 mx-auto">
+            <div class="cta-inner text-center rounded">
+              <h2 class="section-heading mb-4">
+                <span class="section-heading-upper">Cold and Sweet</span>
+                <span class="section-heading-lower">Shakes</span>
+              </h2>
+              
+              
+              <div class="row">
+                <div class="column">
+                  <div class="content">
+                    <img class="img" src="<?php echo base_url('themes/img/Shake-1.jpg');?>" alt="Banana Milkshake" style="width:100%">
+                    <p class="h3font">Banana Milkshake</p>             
+                  </div>
+                </div>
 
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner">
+                <div class="column">
+              <div class="content">
+                <img class="img" src="<?php echo base_url('themes/img/Shake-2.jpg');?>" alt="Cookies N Cream" style="width:100%">
+                <p class="h3font">Cookies N Cream</p>
+              </div>
+                </div>
 
-      <div class="item active">
-         <img class="img" src="<?php echo base_url('themes/img/Shake-1.jpg');?>" alt="Banana Milkshake" style="width:80%">
-        <div class="carousel-caption">
-          <h3>Banana Milkshake</h3>
-        </div>
-      </div>
-
-      <div class="item">
-         <img class="img" src="<?php echo base_url('themes/img/Shake-2.jpg');?>" alt="Cookies N Cream" style="width:100%">
-        <div class="carousel-caption">
-          <h3>Cookies N Cream</h3>
-        </div>
-      </div>
-
-      <div class="item">
-         <img class="img" src="<?php echo base_url('themes/img/Shake-3.png');?>" alt="Peppermint Mocha" style="width:100%">
-        <div class="carousel-caption">
-          <h3>Peppermint Mocha</h3>
-        </div>
-      </div>
-
-            <div class="item">
-         <img class="img" src="<?php echo base_url('themes/img/Shake-4.jpg');?>" alt="Strawberry Smoothie" style="width:100%">
-        <div class="carousel-caption">
-          <h3>Strawberry Smoothie</h3>
-        </div>
-      </div>
-
-      <div class="item">
-         <img class="img" src="<?php echo base_url('themes/img/Shake-5.jpg');?>" alt="Sweet Potato Almond" style="width:100%">
-        <div class="carousel-caption">
-          <h3>Sweet Potato Almond Milkshake</h3>
-        </div>
-      </div>
-
-      <div class="item">
-         <img class="img" src="<?php echo base_url('themes/img/Shake-6.jpg');?>" alt="Thick Chocolate" style="width:100%">
-        <div class="carousel-caption">
-          <h3>Thick Chocolate Milkshake</h3>
-        </div>
-      </div>
-
-      
+                <div class="column">
+              <div class="content">
+                <img class="img" src="<?php echo base_url('themes/img/Shake-3.png');?>" alt="Peppermint Mocha" style="width:100%">
+                <p class="h3font">Peppermint Mocha</p>
+              </div>
+                </div>
   
-    </div>
+                <div class="column">
+              <div class="content">
+                <img class="img" src="<?php echo base_url('themes/img/Shake-4.jpg');?>" alt="Strawberry Milkshake" style="width:100%">
+                <p class="h3font">Strawberry</p>
+              </div>
+                </div>
 
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
+                <div class="column">
+              <div class="content">
+                <img class="img" src="<?php echo base_url('themes/img/Shake-5.jpg');?>" alt="Sweet Potato Almond" style="width:100%">
+                <p class="h3font">Sweet Potato Almond</p>
+              </div>
+                </div>
+
+                <div class="column">
+              <div class="content">
+                <img class="img" src="<?php echo base_url('themes/img/Shake-6.jpg');?>" alt="Thick Chocolate Milkshake" style="width:100%">
+                <p class="h3font">Thick Chocolate Milkshake</p>
+              </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 </div>
-
-<body>
-
-
-    <section class="page-section cta">
+  </div>
+  <div class="col-md-6">
+    <div class="page-section cta">
       <div class="container">
         <div class="row">
           <div class="col-xl-9 mx-auto">
@@ -95,59 +77,59 @@
               <div class="row">
                 <div class="column">
                 <div class="content">
-                <img class="img" src="<?php echo base_url('themes/img/Toppings-1.jpg');?>" alt="Americano" style="width:67%">
+                <img class="img" src="<?php echo base_url('themes/img/Toppings-1.jpg');?>" alt="Americano" style="width:30%">
                <p class="h3font">Chocolate Bar</p>             
             </div>
-        </div>
-
-  <div class="column">
-    <div class="content">
-      <img class="img" src="<?php echo base_url('themes/img/Toppings-2.png');?>" alt="Afforgato" style="width:100%">
-      <p class="h3font">Banana</p>
-    </div>
-  </div>
-
-  <div class="column">
-    <div class="content">
-      <img class="img" src="<?php echo base_url('themes/img/Toppings-3.jpg');?>" alt="Espresso" style="width:67%">
-      <p class="h3font">Melted Chocolate</p>
-    </div>
-  </div>
+          </div>
   
-  <div class="column">
-  <div class="content">
-    <img class="img" src="<?php echo base_url('themes/img/Toppings-4.jpg');?>" alt="Cappuccino" style="width:100%">
-    <p class="h3font">Marshmallow</p>
-  </div>
-</div>
-
-<div class="column">
-  <div class="content">
-    <img class="img" src="<?php echo base_url('themes/img/Toppings-5.jpg');?>" alt="Latte" style="width:100%">
-    <p class="h3font">Oreo</p>
-  </div>
-</div>
-
-<div class="column">
-  <div class="content">
-    <img class="img" src="<?php echo base_url('themes/img/Toppings-7.jpg');?>" alt="Mocha Spiced" style="width:92%">
-    <p class="h3font">Strawberry Stick-O</p>
-  </div>
-</div>
-
-<div class="column">
-  <div class="content">
-    <img class="img" src="<?php echo base_url('themes/img/Toppings-8.jpg');?>" alt="Mocha Spiced" style="width:89%">
-    <p class="h3font">Strawberry</p>
-  </div>
-</div>
-
-<div class="column">
-  <div class="content">
-    <img class="img" src="<?php echo base_url('themes/img/Toppings-9.jpg');?>" alt="Mocha Spiced" style="width:100%">
-    <p class="h3font">Vanilla Ice Cream</p>
-  </div>
-</div>
+          <div class="column">
+            <div class="content">
+              <img class="img" src="<?php echo base_url('themes/img/Toppings-2.png');?>" alt="Afforgato" style="width:42%">
+              <p class="h3font">Banana</p>
+            </div>
+          </div>
+        
+          <div class="column">
+            <div class="content">
+              <img class="img" src="<?php echo base_url('themes/img/Toppings-3.jpg');?>" alt="Espresso" style="width:30%">
+              <p class="h3font">Melted Chocolate</p>
+            </div>
+          </div>
+          
+          <div class="column">
+          <div class="content">
+            <img class="img" src="<?php echo base_url('themes/img/Toppings-4.jpg');?>" alt="Cappuccino" style="width:42%">
+            <p class="h3font">Marshmallow</p>
+          </div>
+        </div>
+        
+        <div class="column">
+          <div class="content">
+            <img class="img" src="<?php echo base_url('themes/img/Toppings-5.jpg');?>" alt="Latte" style="width:42%">
+            <p class="h3font">Oreo</p>
+          </div>
+        </div>
+        
+        <div class="column">
+          <div class="content">
+            <img class="img" src="<?php echo base_url('themes/img/Toppings-7.jpg');?>" alt="Mocha Spiced" style="width:42%">
+            <p class="h3font">Strawberry Stick-O</p>
+          </div>
+        </div>
+        
+        <div class="column">
+          <div class="content">
+            <img class="img" src="<?php echo base_url('themes/img/Toppings-8.jpg');?>" alt="Mocha Spiced" style="width:42%">
+            <p class="h3font">Strawberry</p>
+          </div>
+        </div>
+        
+        <div class="column">
+          <div class="content">
+            <img class="img" src="<?php echo base_url('themes/img/Toppings-9.jpg');?>" alt="Mocha Spiced" style="width:42%">
+            <p class="h3font">Vanilla Ice Cream</p>
+          </div>
+        </div>
 
 
 </div>
@@ -155,4 +137,14 @@
           </div>
         </div>
       </div>
-    </section>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+  </body>
