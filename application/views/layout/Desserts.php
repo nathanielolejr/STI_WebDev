81086a3fce860ec0a1c643b738cc9949c05bd1ed
WarriorPraise <praisewarriorkane@gmail.com>
2018-01-24 13:36:51 +0800

    menu

--- application/views/layout/Desserts.php
+++ application/views/layout/Desserts.php
@@ -15,44 +15,44 @@
               
               
               <div class="row">
-                <div class="column">
+                <div class="hover01 column">
                 <div class="content">
-                <img class="img" src="<?php echo base_url('themes/img/Dessert-1.jpg');?>" alt="Americano" style="width:100%">
+                <figure><img class="img" src="<?php echo base_url('themes/img/Dessert-1.jpg');?>" alt="Americano" style="width:100%"></figure>
                <p class="h3font">Apple Waffle</p>             
             </div>
         </div>
 
-  <div class="column">
+  <div class="hover01 column">
     <div class="content">
-      <img class="img" src="<?php echo base_url('themes/img/Dessert-2.jpg');?>" alt="Afforgato" style="width:100%">
+      <figure><img class="img" src="<?php echo base_url('themes/img/Dessert-2.jpg');?>" alt="Afforgato" style="width:100%"></figure>
       <p class="h3font">Pumpkin Spice Waffle</p>
     </div>
   </div>
 
-  <div class="column">
+  <div class="hover01 column">
     <div class="content">
-      <img class="img" src="<?php echo base_url('themes/img/Dessert-3.jpeg');?>" alt="Espresso" style="width:100%">
+      <figure><img class="img" src="<?php echo base_url('themes/img/Dessert-3.jpeg');?>" alt="Espresso" style="width:100%"></figure>
       <p class="h3font">Caramelized Banana & Pearl Sugar Pancake </p>
     </div>
   </div>
   
-  <div class="column">
+  <div class="hover01 column">
   <div class="content">
-    <img class="img" src="<?php echo base_url('themes/img/Dessert-4.png');?>" alt="Cappuccino" style="width:100%">
+    <figure><img class="img" src="<?php echo base_url('themes/img/Dessert-4.png');?>" alt="Cappuccino" style="width:100%"></figure>
     <p class="h3font">Carrot Coconut Cake</p>
   </div>
 </div>
 
-<div class="column">
+<div class="hover01 column">
   <div class="content">
-    <img class="img" src="<?php echo base_url('themes/img/Dessert-5.jpg');?>" alt="Latte" style="width:100%">
+    <figure><img class="img" src="<?php echo base_url('themes/img/Dessert-5.jpg');?>" alt="Latte" style="width:100%"></figure>
     <p class="h3font">Cinnamon Roll</p>
   </div>
 </div>
 
-<div class="column">
+<div class="hover01 column">
   <div class="content">
-    <img class="img" src="<?php echo base_url('themes/img/Dessert-6.jpg');?>" alt="Mocha Spiced" style="width:100%">
+    <figure><img class="img" src="<?php echo base_url('themes/img/Dessert-6.jpg');?>" alt="Mocha Spiced" style="width:100%"></figure>
     <p class="h3font">Slider Crownie</p>
   </div>
 </div>
