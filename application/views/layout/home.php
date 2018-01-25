d8a9cd05eff33362b5d79c4175d58eb0a203fa13
shamae17 <prettykrisha20@gmail.com>
2018-01-25 11:40:53 +0800

    change home

--- application/views/layout/home.php
+++ application/views/layout/home.php
@@ -1,24 +1,13 @@
 <section class="page-section clearfix" id='home-section'>
       <div class="container">
         <div class="intro">
-          <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="<?php echo base_url('themes/img/home.jpg'); ?>" alt="">
-          <div class="intro-text left-0 text-center bg-faded p-5 rounded">
-            <h2 class="section-heading mb-4">Enjoy the serene sensation 
-            </h2>
-            <p class="mb-3">You'll love our cafe. We love our guest.
-            </p>
-            <div class="intro-button mx-auto">
-              <a class="btn btn-primary btn-xl" href="<?php echo base_url('welcome/contact');?>">Visit Us Today!</a>
+          <!-- <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="<?php //echo base_url('themes/img/home.jpg'); ?>" alt=""> -->
+          <div class="text-center bg-faded p-5 col-md-12 rounded">
+            <div class="row" >
+              <img class="center img-responsive ing-fluid rounded img-fluid" src="<?php echo base_url('themes/img/home.jpg'); ?>" alt="">
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <section class="page-section cta">
-      <div class="container">
-        <div class="row">
-          <div class="col-xl-9 mx-auto">
+            <div class="row">
+              <div class="col-xl-9 mx-auto cta">
             <div class="cta-inner text-center rounded">
               <h2 class="section-heading mb-4">
                 <span class="section-heading-upper">Our Promise</span>
@@ -27,6 +16,19 @@
               <p class="mb-0">Provide a good service with a friendly atmosphere that can be a place to dream as you try to escape the daily stresses of our life and have a comfortable place to meet with your friends. With a growing demand for high-quality gourmet coffee, dessert and great service.</p>
             </div>
           </div>
+            </div>
+            <div class="intro-button mx-auto">
+              <a class="btn btn-primary btn-xl" href="<?php echo base_url('welcome/contact');?>">Visit Us Today!</a>
+            </div>
+          </div>
         </div>
       </div>
     </section>
+<!-- 
+    <section class="page-section cta">
+      <div class="container">
+        <div class="row">
+          
+        </div>
+      </div>
+    </section> -->
