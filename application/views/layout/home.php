a68e8ed8e3cdad103cf6fb3674e461cce68dc01f
shamae17 <prettykrisha20@gmail.com>
2018-01-25 17:21:48 +0800

    changed and added text in footer, home, businesscasualmin

--- application/views/layout/home.php
+++ application/views/layout/home.php
@@ -4,57 +4,33 @@
           <!-- <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="<?php //echo base_url('themes/img/home.jpg'); ?>" alt=""> -->
           <div class="text-center bg-faded p-5 col-md-11 rounded" style="">
             <div class="row" >
+              <div class="container">
+                <div class="intro">
+                  <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="<?php echo base_url('themes/img/home.jpg'); ?>" 
+                  alt="">
+                  <div class="intro-text left-0 text-center bg-faded p-5 rounded">
+                    <h2 class="section-heading mb-4">Enjoy the serene sensation</h2>
+                    <p class="mb-3">You'll love our cafe. We love our guest.</p>
+                    <div class="intro-button mx-auto">
+                      <a class="btn btn-primary btn-xl" href="<?php echo base_url('welcome/login');?>">Join Us Now!</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
 
             </div>
             <div class="row">
               <div class="col-xl-9 mx-auto cta">
-            <div class="cta-inner text-center rounded">
-              <h2 class="section-heading mb-4">
-                <span class="section-heading-upper">Our Promise</span>
-                <span class="section-heading-lower">To You</span>
-              </h2>
-              <p class="mb-0">Provide a good service with a friendly atmosphere that can be a place to dream as you try to escape the daily stresses of our life and have a comfortable place to meet with your friends. With a growing demand for high-quality gourmet coffee, dessert and great service.</p>
+                <div class="cta-inner text-center rounded">
+                  <h2 class="section-heading mb-4">
+                    <span class="section-heading-upper">Our Promise</span>
+                    <span class="section-heading-lower">To You</span>
+                  </h2>
+                  <p class="mb-0">Provide a good service with a friendly atmosphere that can be a place to dream as you try to escape the daily stresses of our life and have a comfortable place to meet with your friends. With a growing demand for high-quality gourmet coffee, dessert and great service.</p>
+                </div>
+              </div>
             </div>
           </div>
-            </div>
-             <div id="fh5co-about" data-section="about">
-      <div class="fh5co-2col fh5co-bg to-animate-2 fadeIn animated imgh">
-      </div>  
-      <div class="fh5co-2col fh5co-text">
-        <h2 class="heading to-animate">
-        <span class="section-heading-lower">Dessert</span>
-        </h2>
-        <p class="to-animate"><span class="firstcharacter">Y</span>ou will never fully satisfy your own desires unless a coffee comes with something sweet - a dessert, made by our best chefs that will melt your tongue in its perfect harmonious blend. Take your obsession to the next level with these jaw-dropping desserts made for coffee lovers. Whether you're layering coffee cream into mousse pots, adding espresso to rich chocolate cake batter to give you the buzz you're looking for.</p>
-        <p class="text-center to-animate"><a href="<?php echo base_url('welcome/Desserts');?>" class="btn btn-primary btn-outline">Explore Our Desserts</a></p>
+        </div>
       </div>
-    </div>
-     <div id="fh5co-about" data-section="about">
-      <div class="fh5co-2col fh5co-bg to-animate-2 fadeIn animated imgh">
-      </div>  
-      <div class="fh5co-2col fh5co-text">
-        <h2 class="heading to-animate">
-        <span class="section-heading-lower">Dessert</span>
-        </h2>
-        <p class="to-animate"><span class="firstcharacter">Y</span>ou will never fully satisfy your own desires unless a coffee comes with something sweet - a dessert, made by our best chefs that will melt your tongue in its perfect harmonious blend. Take your obsession to the next level with these jaw-dropping desserts made for coffee lovers. Whether you're layering coffee cream into mousse pots, adding espresso to rich chocolate cake batter to give you the buzz you're looking for.</p>
-        <p class="text-center to-animate"><a href="<?php echo base_url('welcome/Desserts');?>" class="btn btn-primary btn-outline">Explore Our Desserts</a></p>
-      </div>
-    </div>
-<<<<<<< HEAD
-     <div id="fh5co-about" data-section="about">
-      <div class="fh5co-2col fh5co-bg to-animate-2 fadeIn animated imgh">
-      </div>  
-      <div class="fh5co-2col fh5co-text">
-        <h2 class="heading to-animate">
-        <span class="section-heading-lower">Dessert</span>
-        </h2>
-        <p class="to-animate"><span class="firstcharacter">Y</span>ou will never fully satisfy your own desires unless a coffee comes with something sweet - a dessert, made by our best chefs that will melt your tongue in its perfect harmonious blend. Take your obsession to the next level with these jaw-dropping desserts made for coffee lovers. Whether you're layering coffee cream into mousse pots, adding espresso to rich chocolate cake batter to give you the buzz you're looking for.</p>
-        <p class="text-center to-animate"><a href="<?php echo base_url('welcome/Desserts');?>" class="btn btn-primary btn-outline">Explore Our Desserts</a></p>
-      </div>
-    </div>
-  </div>
-=======
-</div>
->>>>>>> aa3b95e8544f1318b715dfe67b3765935761b07c
-</div>
-</div>
-</section>
+    </section>
