633a256d31126becaeba524130e434c3a28eb1f1
DoraemonOppa <registristathea@gmail.com>
2018-01-26 14:23:56 +0800

    master home changed pictures, added css and added pictures

--- application/views/layout/home.php
+++ application/views/layout/home.php
@@ -7,22 +7,22 @@
 
               <div class="container">
                 <div class="intro">
-                  <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="<?php echo base_url('themes/img/home.jpg'); ?>" 
+                  <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="<?php echo base_url('themes/img/collage2.jpg'); ?>" 
                   alt="">
                   <div class="intro-text left-0 text-center bg-faded p-5 rounded">
                     <h2 class="section-heading mb-4">Enjoy the serene sensation</h2>
-                    <p class="mb-3">Start the day with great taste</p>
+                    <p class="mb-3">You'll love our cafe. We love our guest.</p>
                     <div class="intro-button mx-auto">
-                      <a class="btn btn-primary btn-xl" href="<?php echo base_url('welcome/login');?>">Join Us Now!</a>
+                      <a class="btn btn-primary btn-xl" href="<?php echo base_url('welcome/menu');?>">Explore Our Menu</a>
                     </div>
                   </div>
                 </div>
               </div>
 
             </div>
-           
-            <div class="">
-              <div class="col-xl-13 mx-auto cta">
+            
+            <div class="row">
+              <div class="col-xl-9 mx-auto cta">
                 <div class="cta-inner text-center rounded">
                   <h2 class="section-heading mb-4">
                     <span class="section-heading-upper">Our Promise</span>
@@ -30,6 +30,9 @@
                   </h2>
                   <p class="mb-0">Provide a good service with a friendly atmosphere that can be a place to dream as you try to escape the daily stresses of our life and have a comfortable place to meet with your friends. With a growing demand for high-quality gourmet coffee, dessert and great service.</p>
                 </div>
+                <div class="intro-button mx-auto">
+                      <a class="btn btn-primary btn-xl" href="<?php echo base_url('welcome/login');?>">Join Us Now!</a>
+                    </div>
               </div>
             </div>
           </div>
