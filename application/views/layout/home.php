af1624a3f7b8e4dad4950211bc0d20d1552d3bee
Keim Flor <Keim.flor@gmail.com>
2018-01-26 10:53:37 +0800

    Master home change background color

--- application/views/layout/home.php
+++ application/views/layout/home.php
@@ -2,7 +2,7 @@
       <div class="container">
         <div class="intro" style="padding-left: 8%;">
           <!-- <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="<?php //echo base_url('themes/img/home.jpg'); ?>" alt=""> -->
-          <div class="text-center bg-faded p-5 col-md-11 rounded" style="">
+          <div class="text-center bg-faded bg-custom p-5 col-md-11 rounded" style="">
             <div class="row" >
 
               <div class="container">
@@ -11,7 +11,7 @@
                   alt="">
                   <div class="intro-text left-0 text-center bg-faded p-5 rounded">
                     <h2 class="section-heading mb-4">Enjoy the serene sensation</h2>
-                    <p class="mb-3">You'll love our cafe. We love our guest.</p>
+                    <p class="mb-3">Start the day with great taste</p>
                     <div class="intro-button mx-auto">
                       <a class="btn btn-primary btn-xl" href="<?php echo base_url('welcome/login');?>">Join Us Now!</a>
                     </div>
@@ -20,9 +20,9 @@
               </div>
 
             </div>
-            
-            <div class="row">
-              <div class="col-xl-9 mx-auto cta">
+           
+            <div class="">
+              <div class="col-xl-13 mx-auto cta">
                 <div class="cta-inner text-center rounded">
                   <h2 class="section-heading mb-4">
                     <span class="section-heading-upper">Our Promise</span>
